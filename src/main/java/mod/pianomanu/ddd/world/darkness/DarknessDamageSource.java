789596b2a6c3a8ad541220371022ba064c024635
Manuel <jirsakm@web.de>
2021-05-15 17:52:19 +0200

    Some tests...

---
+++ src/main/java/mod/pianomanu/ddd/world/darkness/DarknessDamageSource.java
@@ -0,0 +1,20 @@
+package mod.pianomanu.ddd.world.darkness;
+
+import net.minecraft.util.DamageSource;
+
+//TODO
+
+/**
+ * Will add description later...
+ *
+ * @author PianoManu
+ * @version 1.0 05/15/21
+ */
+public class DarknessDamageSource extends DamageSource {
+    public static final DarknessDamageSource DAMAGE_BY_DARKNESS = (DarknessDamageSource) (new DarknessDamageSource("darkness")).bypassArmor();
+
+    public DarknessDamageSource(String name) {
+        super(name);
+    }
+}
+//========SOLI DEO GLORIA========//
