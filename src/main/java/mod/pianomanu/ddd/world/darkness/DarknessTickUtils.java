7bea4ca03d1ded8f1aa031c081bcc283e950f130
Manuel <jirsakm@web.de>
2021-05-21 14:44:32 +0200

    Fixed server crash because of EntityData problems

--- src/main/java/mod/pianomanu/ddd/world/darkness/DarknessTickUtils.java
+++ src/main/java/mod/pianomanu/ddd/world/darkness/DarknessTickUtils.java
@@ -2,12 +2,13 @@ package mod.pianomanu.ddd.world.darkness;
 
 import mod.pianomanu.ddd.DDDMain;
 import mod.pianomanu.ddd.config.DDDConfig;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.network.datasync.DataParameter;
-import net.minecraft.network.datasync.DataSerializers;
-import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Will add description later...
@@ -16,7 +17,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
  * @version 1.3 05/21/21
  */
 public class DarknessTickUtils {
-    private static final DataParameter<Integer> TIME_IN_DARKNESS = EntityDataManager.defineId(ServerPlayerEntity.class, DataSerializers.INT);
+    private final Map<PlayerEntity, Integer> players = new HashMap<>();
 
     @SubscribeEvent
     public void tick(final TickEvent.PlayerTickEvent event) {
@@ -25,10 +26,9 @@ public class DarknessTickUtils {
             if (player.level.dimension().equals(DDDMain.DEEP_DARK_DIMENSION) && DDDConfig.MAX_TIME_IN_DARKNESS_BEFORE_RECEIVING_DAMAGE.get() > -1) {
                 int time = 0;
                 try {
-                    time = player.getEntityData().get(TIME_IN_DARKNESS);
+                    time = players.get(player);
                 } catch (NullPointerException e) {
-                    player.getEntityData().define(TIME_IN_DARKNESS, 0);
-                    player.getEntityData().set(TIME_IN_DARKNESS, time);
+                    players.put(player, 0);
                 }
                 if (player.getBrightness() < 0.05 && time < DDDConfig.MAX_TIME_IN_DARKNESS_BEFORE_RECEIVING_DAMAGE.get()) {
                     time++;
@@ -38,7 +38,7 @@ public class DarknessTickUtils {
                 if (time == DDDConfig.MAX_TIME_IN_DARKNESS_BEFORE_RECEIVING_DAMAGE.get()) {
                     player.hurt(DarknessDamageSource.DAMAGE_BY_DARKNESS, DDDConfig.DAMAGE_PER_HIT_RECEIVED_BY_DARKNESS.get().floatValue());
                 }
-                player.getEntityData().set(TIME_IN_DARKNESS, time);
+                players.put(player, time);
             }
         }
     }
